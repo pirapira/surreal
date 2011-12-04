aae938204345558c8d58febbd09168c340943e6e
Yoichi Hirai <yh@is.s.u-tokyo.ac.jp>
2011-12-04 20:51:16 +0100

    be in bool.

--- surreal.v
+++ surreal.v
@@ -33,74 +33,48 @@ Qed.
 
 *)
 
-Definition gle: forall m n, game m -> game n -> Prop.
-intros m n.
-remember (m + n) as s. 
-assert (m + n <= s).
-rewrite Heqs.
-auto.
-clear Heqs.
-generalize m n H.
-clear m n H.
-induction s.
 
-intros.
+Inductive gle: forall m n, game m -> game n -> bool -> Prop :=
+| gle_Sli: forall m n g g' b,
+  gle m n g g' b ->
+  gle (S m) n (inherit _ g) g' b
+| gle_Sri: forall m n g g' b,
+  gle m n g g' b ->
+  gle _ (S n) g (inherit _ g') b
+| gle_St: forall m n (xL: game m -> Prop) xR yL (yR: game n -> Prop),
+  (forall xl, xL xl -> gle (S n) _ (compose (game n) yL yR) xl false) ->
+  (forall yr, yR yr -> gle _ (S m) yr (compose (game m) xL xR) false) ->
+  gle (S m) (S n) (compose (game m) xL xR) (compose (game n) yL yR) true
+| gle_Sfl: forall m n (xL: game m -> Prop) xR yL (yR: game n -> Prop),
+  (exists xl, xL xl /\ gle (S n) _ (compose (game n) yL yR) xl true) ->
+  gle (S m) (S n) (compose (game m) xL xR) (compose (game n) yL yR) false
+| gle_Sfr: forall m n (xL: game m -> Prop) xR yL (yR: game n -> Prop),
+  (exists yr, yR yr /\ gle _ (S m) yr (compose (game m) xL xR) false) ->
+  gle (S m) (S n) (compose (game m) xL xR) (compose (game n) yL yR) false
+.
+
+
+Lemma refl: forall m (g: game m), gle _ _ g g true.
+induction m.
+intro g.
+destruct g.
+
+intro g.
+destruct g; fold game in *.
+apply gle_Sli.
+apply gle_Sri.
+apply IHm.
+
+rename P into L.
+rename P0 into R.
+apply gle_St.
+intro xl.
+intro xlL.
 destruct m.
-inv X.
-exact (False). (* never reached *)
+inv xl.
+destruct xl.
+fold game in *.
 
-intros m n.
-intro small.
-
-intros x y.
-destruct m.
-inv x.
-
-destruct n.
-inv y.
-
-case_eq x; fold game in *.
-intro x_content.
-intro x_eq.
-clear x_eq x.
-
-gen IHs m.
-gen IHs (S n).
-refine (IHs _ x_content y).
-Require Import Omega.
-omega.
-
-idtac.
-intros xL xR.
-intro x_eq.
-
-case_eq y; fold game in *.
-intro y_content.
-intro y_eq.
-clear y y_eq.
-clear xL xR x_eq.
-refine (IHs _ _ _ x y_content).
-omega.
-
-intros yL yR.
-intro y_eq.
-clear x_eq y_eq.
-refine (_ /\ _).
-gen IHs (S n).
-gen IHs m.
-assert (S n + m <= s).
-omega.
-exact (forall (xl: game m), xL xl -> IHs H y xl -> False).
-
-clear xL xR y y yL.
-gen IHs n.
-gen IHs (S m).
-assert (n + S m <= s).
-omega.
-exact (forall (yr: game n), yR yr -> IHs H yr x -> False).
-Defined.
-
-Check gle.
 
 Definition charm: game 0 -> Prop.
 intro.
@@ -171,38 +145,3 @@ Lemma refl: forall m g, gle m m g g.
   simpl.
 
 
-(* one nice way 
-Inductive gle: forall m n, game m -> game n -> bool -> Prop :=
-| gle_Sli: forall m n g g' b,
-  gle m n g g' b ->
-  gle (S m) n (inherit _ g) g' b
-| gle_Sri: forall m n g g' b,
-  gle m n g g' b ->
-  gle _ (S n) g (inherit _ g') b
-| gle_St: forall m n (xL: game m -> Prop) xR yL (yR: game n -> Prop),
-  (forall xl, xL xl -> gle (S n) _ (compose (game n) yL yR) xl false) ->
-  (forall yr, yR yr -> gle _ (S m) yr (compose (game m) xL xR) false) ->
-  gle (S m) (S n) (compose (game m) xL xR) (compose (game n) yL yR) true
-| gle_Sfl: forall m n (xL: game m -> Prop) xR yL (yR: game n -> Prop),
-  (exists xl, xL xl /\ gle (S n) _ (compose (game n) yL yR) xl true) ->
-  gle (S m) (S n) (compose (game m) xL xR) (compose (game n) yL yR) false
-| gle_Sfr: forall m n (xL: game m -> Prop) xR yL (yR: game n -> Prop),
-  (exists yr, yR yr /\ gle _ (S m) yr (compose (game m) xL xR) false) ->
-  gle (S m) (S n) (compose (game m) xL xR) (compose (game n) yL yR) false
-.
-*)
-
-
-Lemma refl: forall m (g: game m), gle _ _ g g true.
-induction m.
-intro g.
-destruct g.
-
-intro g.
-destruct g; fold game in *.
-apply gle_Sli.
-apply gle_Sri.
-apply IHm.
-
-rename P into L.
-rename P0 into R.
