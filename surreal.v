7d1953ecc65558bdfb41db1f945ed7e32117c62d
Yoichi Hirai <yh@is.s.u-tokyo.ac.jp>
2011-12-04 20:43:28 +0100

    stuck at reflexivity.

--- surreal.v
+++ surreal.v
@@ -1,11 +1,11 @@
-Inductive step (orig: Set): Set :=
+Inductive step (orig: Type): Type :=
   | inherit: orig -> step orig
-  | compose: (orig -> bool) -> (orig -> bool) -> step orig
+  | compose: (orig -> Prop) -> (orig -> Prop) -> step orig
 .
 
-Inductive empty: Set := .
+Inductive empty: Type := .
 
-Fixpoint game (m: nat): Set :=
+Fixpoint game (m: nat): Type :=
   match m with
    | O => empty
    | S m' => step (game m')
@@ -32,8 +32,146 @@ Lemma empty_uniq: forall
 Qed.
 
 *)
-  
 
+Definition gle: forall m n, game m -> game n -> Prop.
+intros m n.
+remember (m + n) as s. 
+assert (m + n <= s).
+rewrite Heqs.
+auto.
+clear Heqs.
+generalize m n H.
+clear m n H.
+induction s.
+
+intros.
+destruct m.
+inv X.
+exact (False). (* never reached *)
+
+intros m n.
+intro small.
+
+intros x y.
+destruct m.
+inv x.
+
+destruct n.
+inv y.
+
+case_eq x; fold game in *.
+intro x_content.
+intro x_eq.
+clear x_eq x.
+
+gen IHs m.
+gen IHs (S n).
+refine (IHs _ x_content y).
+Require Import Omega.
+omega.
+
+idtac.
+intros xL xR.
+intro x_eq.
+
+case_eq y; fold game in *.
+intro y_content.
+intro y_eq.
+clear y y_eq.
+clear xL xR x_eq.
+refine (IHs _ _ _ x y_content).
+omega.
+
+intros yL yR.
+intro y_eq.
+clear x_eq y_eq.
+refine (_ /\ _).
+gen IHs (S n).
+gen IHs m.
+assert (S n + m <= s).
+omega.
+exact (forall (xl: game m), xL xl -> IHs H y xl -> False).
+
+clear xL xR y y yL.
+gen IHs n.
+gen IHs (S m).
+assert (n + S m <= s).
+omega.
+exact (forall (yr: game n), yR yr -> IHs H yr x -> False).
+Defined.
+
+Check gle.
+
+Definition charm: game 0 -> Prop.
+intro.
+inv X.
+Defined.
+
+Definition zero: game 1 := compose _ charm charm.
+Definition one : game 2 := compose _ (fun _ => True) (fun _ => False).
+Definition half: game 3 := compose _ (fun x => x = inherit _ zero) (fun y => y = one).
+
+Lemma zero_one: gle _ _ zero one.
+  compute.
+  intuition.
+Qed.
+
+Lemma one_zero: gle _ _ one zero -> False.
+  compute.
+  intuition.
+  clear H1.
+  gen H0 zero.
+  apply H0.
+  auto.
+  compute.
+  intuition.
+Qed.
+
+Lemma zero_zero: gle _ _ zero zero.
+  compute.
+  intuition.
+Qed.
+
+Lemma one_one: gle _ _ one one.
+  compute.
+  intuition.
+  destruct xl.
+  inv e.
+  intuition.
+  clear H2.
+  apply H1 with zero.
+  auto.
+  compute.
+  intuition.
+Qed.
+
+Lemma zero_half: gle _ _ zero half.
+  compute.
+  intuition.
+  subst.
+  intuition.
+  clear H1.
+  gen H zero.
+  intuition.
+  apply H0.
+  compute.
+  intuition.
+Qed.
+  
+(* next, reflexivity of gle *)
+Lemma refl: forall m g, gle m m g g.
+  induction m.
+  intros.
+  inv g.
+
+  intros.
+  destruct g; fold game in *.
+
+  unfold gle.
+  simpl.
+
+
+(* one nice way 
 Inductive gle: forall m n, game m -> game n -> bool -> Prop :=
 | gle_Sli: forall m n g g' b,
   gle m n g g' b ->
@@ -41,188 +179,30 @@ Inductive gle: forall m n, game m -> game n -> bool -> Prop :=
 | gle_Sri: forall m n g g' b,
   gle m n g g' b ->
   gle _ (S n) g (inherit _ g') b
-| gle_S: forall m n xL xR yL yR,
-  (forall xl, xL xl = true -> gle (S n) _ (compose (game n) yL yR) xl false) ->
-  (* add the other condition *)
+| gle_St: forall m n (xL: game m -> Prop) xR yL (yR: game n -> Prop),
+  (forall xl, xL xl -> gle (S n) _ (compose (game n) yL yR) xl false) ->
+  (forall yr, yR yr -> gle _ (S m) yr (compose (game m) xL xR) false) ->
   gle (S m) (S n) (compose (game m) xL xR) (compose (game n) yL yR) true
+| gle_Sfl: forall m n (xL: game m -> Prop) xR yL (yR: game n -> Prop),
+  (exists xl, xL xl /\ gle (S n) _ (compose (game n) yL yR) xl true) ->
+  gle (S m) (S n) (compose (game m) xL xR) (compose (game n) yL yR) false
+| gle_Sfr: forall m n (xL: game m -> Prop) xR yL (yR: game n -> Prop),
+  (exists yr, yR yr /\ gle _ (S m) yr (compose (game m) xL xR) false) ->
+  gle (S m) (S n) (compose (game m) xL xR) (compose (game n) yL yR) false
 .
+*)
 
 
-
-
-
-
-
-
-
-
-
-
-Definition _gle: forall s, forall m n, m + n <= s -> game m -> game n -> bool.
-induction s.
-intros.
-exact (true).
-
-intros m n H x y.
-destruct m; destruct n.
-exact (true).
-
-case_eq y.
-fold game.
-intro y_content.
-intro y_eq.
-refine (IHs 0 n _ x y_content).
-Require Omega.
-omega.
-
-fold game.
-intros L R.
-intro y_eq.
-clear y_eq.
-clear L.
-rename R into yR.
-gen IHs n.
-gen IHs O.
-assert (n + 0 <= s).
-omega.
-gen IHs H0.
-clear H0.
-generalize ok_yRx.
-intro ok.
-gen ok (game O).
-gen ok (game n).
-exact (ok x yR IHs).
-
-case_eq x.
-fold game.
-intro x_content.
-intro x_eq.
-gen IHs m.
-gen IHs O.
-apply IHs.
-omega.
-exact x_content.
-exact y.
-fold game.
-intros xL xR.
-intro x_eq.
-generalize ok_yxL.
-intro ok.
-gen ok (game m).
-gen ok (game O).
-refine (ok xL y _ ).
-gen IHs O.
-gen IHs m.
-apply IHs.
-omega.
-
-case_eq x; fold game; case_eq y; fold game.
-intro y_content.
-intro y_eq.
-clear y y_eq.
-intro x_content.
-intro x_eq.
-clear x_eq x.
-gen IHs m.
-gen IHs n.
-apply IHs.
-omega.
-exact x_content.
-exact y_content.
-
-
-intros yL yR.
-intro y_eq.
-clear yL yR y_eq.
-intro x_content.
-intro x_eq.
-clear x x_eq.
-gen IHs m.
-gen IHs (S n).
-apply IHs.
-omega.
-exact x_content.
-exact y.
-
-intro y_content.
-intro y_eq.
-clear y_eq y.
-intros _ _ _.
-gen IHs (S m).
-gen IHs n.
-apply IHs.
-omega.
-exact x.
-exact y_content.
-
-intros yL yR.
-intro y_eq.
-intros xL xR.
-intro x_eq.
-
-refine (andb _ _); [ generalize ok_yxL | generalize ok_yRx ].
-intro ok.
-clear x_eq y_eq xR x yL yR.
-gen ok (game m).
-gen ok (game (S n)).
-apply ok.
-apply xL.
-apply y.
-apply IHs.
-omega.
-
-clear x_eq xL xR.
-clear y_eq y yL.
-intro ok.
-gen ok (game (S m)).
-gen ok (game n).
-apply ok.
-apply x.
-apply yR.
-apply IHs.
-omega.
-Defined.
-
-Lemma plpl: forall m n,
-  m + n <= m + n.
-  intros.
-  omega.
-Qed.
-
-Definition gle { m n } (g: game m) (g' :game n): bool :=
-  _gle (m + n) m n (plpl m n) g g'.
-
-Definition zero : game O := tt.
-
-Definition one : game (S O) := compose unit (zero :: nil) nil.
-Definition neg_one : game (S O) := compose unit nil (zero :: nil).
-
-Eval compute in (gle one zero). (* false *)
-Eval compute in (gle zero zero). (* true *)
-Eval compute in (gle neg_one zero). (* true *)
-Eval compute in (gle one zero). (* false *)
-
-Print _gle.
-
-Lemma inhL: forall m n (Sg: game (S n)) (g: game n) (g': game m),
-  gle (sg n (inherit (game n) g)) g' = gle g g'.
-intros.
-unfold sg.
-unfold gle.
-destruct m.
-destruct g'.
-
-
-
-Lemma refl: forall m (g: game m), gle g g = true.
+Lemma refl: forall m (g: game m), gle _ _ g g true.
 induction m.
-
-(* base case *)
 intro g.
 destruct g.
-reflexivity.
 
-(* S *)
 intro g.
-destruct g.
-fold game in *.
+destruct g; fold game in *.
+apply gle_Sli.
+apply gle_Sri.
+apply IHm.
+
+rename P into L.
+rename P0 into R.
